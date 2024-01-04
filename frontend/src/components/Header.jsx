edf6eef4c2dee5765d5ca5b88a306a9ccb16c0b6
Irenej Bozovicar <irenej@bambuser.com>
2024-01-04 14:27:24 +0100

    Change text in the header based on what page user is located on

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,35 @@
 // Header.jsx
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+  // Get the current path
+  const location = useLocation();
+
+  // Define the text for different paths
+  const text = {
+    "/word-in-specific-youtube-video": {
+      h1: "Find a word in a YouTube video of your choice",
+      p: "Words in specific action: Explore Your Vocabulary in Every Clip"
+    },
+    "/word-in-random-youtube-video": {
+      h1: "Find a word in a random YouTube video",
+      p: "Words in Action: Explore Your Vocabulary in Every Clip"
+    }
+  };
+
+  // Get the text for the current path or use a default value
+  const { h1, p } = text[location.pathname] || {
+    h1: "Find a word in a YouTube video",
+    p: "Words in Any Action: Explore Your Vocabulary in Any Clip"
+  };
+
   return (
     <div className="py-5 text-center"> {/* Add padding and centering classes */}
       <Container>
-        <h1>Find a word in a random YouTube video</h1>
-        <p>Words in Action: Explore Your Vocabulary in Every Clip</p>
+        <h1>{h1}</h1>
+        <p>{p}</p>
       </Container>
     </div>
   );
