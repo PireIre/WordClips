7ac097dd2eea59fefc65acf189f49cb7e532a830
Irenej Bozovicar <irenej@bambuser.com>
2024-01-04 14:51:05 +0100

    Change Placeholder Img depending on page user is located on

--- frontend/src/components/VideoPlaceholder.jsx
+++ frontend/src/components/VideoPlaceholder.jsx
@@ -2,10 +2,17 @@
 import React from 'react';
 import './VideoPlaceholder.css';
 import blurredImage from '../assets/blurred.png';
+import blurredImage2 from '../assets/blurred_2.png';
 import notFoundImage from '../assets/404.png';
 
+const displayedPlaceholderImage = {
+  "/word-in-specific-youtube-video": blurredImage2,
+  "/word-in-random-youtube-video": blurredImage
+};
+
 const VideoPlaceholder = ({ placeholderText }) => {
-  const backgroundImage = placeholderText === '404' ? notFoundImage : blurredImage;
+
+  const backgroundImage = placeholderText === '404' ? notFoundImage : (displayedPlaceholderImage[location.pathname]);
 
   return (
     <div
