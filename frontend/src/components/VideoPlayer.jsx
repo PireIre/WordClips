03e0a0b0e8c01aa9cec24a9cac2968230319b137
Irenej Bozovicar <irenej@bambuser.com>
2024-01-05 10:50:44 +0100

    make videoPlayer component mobile responsive

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -1,19 +1,16 @@
-// src/components/VideoPlayer.js
+// VideoPlayer.js
 import React, { useRef, useState, useEffect } from 'react';
 import './VideoPlayer.css';
-import blurredImage from '../assets/blurred.png';
-
 
 const VideoPlayer = ({ videoId, startTime, searchResults, clipIndex }) => {
   const playerRef = useRef(null);
 
   return (
-    <div>
+    <div className="video-player-container">
       <iframe
         ref={playerRef}
+        className="video-player"
         title="Video Snippet"
-        width="800"
-        height="450"
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1&start=${startTime}&fs=0&rel=0&controls=1`}
         frameBorder="0"
         allowFullScreen
