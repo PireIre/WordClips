c2bfda7b02aa3e49e4d59293661dfe0d01a81df9
Irenej Bozovicar <irenej@bambuser.com>
2024-01-08 14:52:10 +0100

    Change default functions URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // frontend/src/config.js
 const config = {
-    apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+    apiUrl: import.meta.env.VITE_API_URL || 'https://us-central1-wordclips-24acd.cloudfunctions.net/api',
   };
   
   export default config;
